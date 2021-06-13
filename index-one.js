d100d73e7a368def86352c178c366cad01271c0f
andres <andresponton21@yahoo.com>
2021-06-12 22:57:53 -0400

    adding jsonplace

---
+++ index-one.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // getting the html values
+    let yourName = document.querySelector('#your-name')
+    let partnerName = document.querySelector('#partner-name')
+    const evaluateButton = document.querySelector('#evaluate')
+    const successRate = document.querySelector('.rate')
+    const result = document.querySelector('.result')
+    let evalForm = document.querySelector('#evalForm')
+    let textResult = document.querySelector('.textResult')
+
+   // adding an event listener to the form
+    evaluateButton.addEventListener('click', (event)=>{
+        event.preventDefault();
+        // works only if inputs have values
+        if( yourName.value && partnerName.value ){
+        //invoke the getMatch function defined below using input values as parameters
+            getMatch(yourName.value, partnerName.value);
+            textResult.innerHTML = 
+            `These are the results for "${yourName.value.toUpperCase()}" and "${partnerName.value.toUpperCase()}"`
+        // reseting the form's input
+            evalForm.reset()
+        // timeout function encouraging the user to try again
+            setTimeout(() => {
+            textResult.innerHTML='Try different names!!!'
+                
+            }, 5000);
+        }
+        
+    })
+
+
+   function getMatch(name1, name2){
+    fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${name1}&sname=${name2}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "d01bef06d8msh65491a939b54f7cp109236jsn8b8d29335c4d",
+            "x-rapidapi-host": "love-calculator.p.rapidapi.com"
+            
+        }
+    })
+    // getting response and appliying json method
+    .then(response => response.json())
+    .then(response => {
+        // displaying data in document
+        successRate.innerHTML = `Your possible success is: ${response.percentage}%`
+        result.innerHTML = `"${response.result}"`
+        
+        // timeout function encouraging the user to try again
+
+        setTimeout(() => {
+            successRate.innerHTML =''
+            result.innerHTML = ''
+        }, 4500);
+    })
+    // catch function for possible errors
+    .catch(err => {
+        console.error(err);
+    });
+   }
+    
+
+
+})
---
+++ index-two.js
@@ -0,0 +1,38 @@
+const subscribeForm = document.querySelector('#subscirbe-form')
+const friendName = document.querySelector('.friend-name')
+const friendEmail = document.querySelector('.friend-email')
+const subscribeButton = document.querySelector('.subscribe')
+const showName = document.querySelector('.show-name')
+const showEmail = document.querySelector('.show-email')
+
+subscribeForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        body:JSON.stringify({
+                title: friendName.value,
+                body: friendEmail.value,  
+            }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+          
+        })
+        .then(response => response.json())
+        .then(data => {
+            showName.textContent = `Thaks for sharing with: "${data.title}"`
+            showEmail.textContent = `An invitation email with be sent to: "${data.body}"`
+        })
+        setTimeout(() => {
+            showName.textContent = ''
+            showEmail.textContent = ''
+                
+            }, 5000);
+    
+    subscribeForm.reset()
+})
+
+
+
+
